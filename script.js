6c7510d8511e2f587b619cc82b08e8db92b7962c
PatrickCleary <46229349+PatrickCleary@users.noreply.github.com>
2020-08-27 15:53:30 -0400

    add meta-info

--- script.js
+++ script.js
@@ -2,8 +2,8 @@
 // @name         New Userscript
 // @namespace    http://tampermonkey.net/
 // @version      0.1
-// @description  try to take over the world!
-// @author       You
+// @description  Adds functionality to zoom.us/join website
+// @author       Patrick Cleary
 // @match        https://zoom.us/join
 // @require      https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js
 // @grant GM_setValue
